fe24f7e6bec515c82c7dda2d8f2338108cb53a35
Q7A2 <125273639+Q7A2@users.noreply.github.com>
2023-02-15 22:32:17 +0800

    Update GitTest.java

--- src/main/java/com/atQ_A/git/GitTest.java
+++ src/main/java/com/atQ_A/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello Git3 master");
         System.out.println("hello Git3 hot-fix");
         System.out.println("push-test");
+        System.out.println("pull-test");
     }
 }
